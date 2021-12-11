201ae52882f3093d562c9c592441a09f4ef77c43
Santiago Santa <santiagosantaa9@gmail.com>
2021-12-11 10:50:17 -0500

    Actualización funcion cleandata

--- cleandata.py
+++ cleandata.py
@@ -2,7 +2,7 @@ def cleandata(file):
     import numpy as np
     import pandas as pd
 
-    with open('JI_GitHub_SIATA/estacion_data_calidadaire_48_20190101_20201231.txt') as f:
-        station48 = f.read().splitlines() #lista con el nombre de los archivos csv
+    with open("JI_GitHub_SIATA/"+file) as f:
+        station = f.read().splitlines() #lista con el nombre de los archivos csv
     
-    print(station48)
+    print(station)
