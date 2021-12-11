55c03a69978b38a20ae5cab91bb36efeacd934fe
Santiago Santa <santiagosantaa9@gmail.com>
2021-12-11 10:29:30 -0500

    Se crea una funcione para limpiar datos.

---
+++ cleandata.py
@@ -0,0 +1,7 @@
+def cleandata(file):
+    import numpy as np
+    import pandas as pd
+
+    with open('JI_GitHub_SIATA/estacion_data_calidadaire_48_20190101_20201231.txt') as f:
+    station48 = f.read().splitlines() #lista con el nombre de los archivos csv
+    print(station48)
